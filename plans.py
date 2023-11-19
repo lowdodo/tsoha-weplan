cf1295889d74d099b73df036e8ff927b83cdf78c
lowdodo <lowdodo@ad.helsinki.fi>
2023-11-19 23:37:34 +0200

    added different plan functions

--- plans.py
+++ plans.py
@@ -1,13 +1,37 @@
 from db import db
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
-#plans should be added to the main page while session.name 
-#maybe change the order later on butletssee
 def get_allplans():
-    sql = text("SELECT id, name FROM plans ORDER BY name")
+    sql = text("SELECT id, name FROM plans WHERE visible=1 ORDER BY name")
     return db.session.execute(sql).fetchall()
 
-
-#choosing one plan to look into. The plan creator still amiss
 def choose_plan(plan_id):
-    sql = """SELECT plan.name, user.name FROM palns, users WHERE plan.id =: plan_id AND plan.creator_id = user.id"""
-    return db.session.execute(sql, {"plan_id":plan_id}).fetchone()
+    sql = text("SELECT plan.name, user.name, plan.description FROM plans, users WHERE plan.id = :plan_id AND plan.creator_id = user.user_id")
+    return db.session.execute(sql, {"plan_id": plan_id}).fetchone()
+
+def add_plan(creator_id, name, description, newsubplans):
+    sql = text("INSERT INTO plans (creator_id, name, description, visible) VALUES (:creator_id, :name, :description, 1) RETURNING id")
+    plan_id = db.session.execute(sql, {"creator_id": creator_id, "name": name, "description": description}).fetchone()[0]
+
+    if newsubplans:
+        for subplan in newsubplans:
+            sql = text("INSERT INTO subplans (plan_id, subplan) VALUES (:plan_id, :subplan)")
+            db.session.execute(sql, {"plan_id": plan_id, "subplan": subplan})
+
+    db.session.commit()
+    return plan_id
+
+def remove_plan(plan_id, user_id):
+    sql = text("UPDATE plans SET visible=0 WHERE id=:plan_id AND creator_id=:user_id")
+    db.session.execute(sql, {"plan_id": plan_id, "user_id": user_id})
+    db.session.commit()
+
+def remove_subplan(subplan_id, user_id):
+    sql = text("UPDATE subplans SET visible=0 WHERE id=:subplan_id AND creator_id=:user_id")
+    db.session.execute(sql, {"subplan_id": subplan_id, "user_id": user_id})
+    db.session.commit()
+
+def get_plan_info(plan_id):
+    sql = text("SELECT creator_id, name, description FROM plans WHERE id=:plan_id")
+    return db.session.execute(sql, {"plan_id": plan_id}).fetchone()
