df31dbc4f57f29e252238ee27abbd23a9b46b87f
lowdodo <lowdodo@ad.helsinki.fi>
2023-11-19 23:36:03 +0200

    added add_plan, fixed login/logout

--- routes.py
+++ routes.py
@@ -1,28 +1,30 @@
-from flask import Flask, session, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-import users
+from flask import session, render_template, request, redirect
+import users, plans
+from users import user_id_forname
 from app import app
 
 #First page we see, later we'll make this real pretty alright
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", plans=plans.get_allplans())
 
 #login todo: check the name n pw
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    name = request.form["name"]
-    password = request.form["password"]
-    # todo: checking the name and password
-    session["name"] = name
-    value = session["name"]
+    if request.method == "GET":
+        return render_template("index.html")
+    
+    if request.method == "POST":
+        name = request.form["name"]
+        password = request.form["password"]
+    if not users.login(name, password):
+        return render_template("error.html", message="Incorrect login information")
     return redirect("/")
 
 #logout
 @app.route("/logout")
 def logout():
-    del session["name"]
-    del session["user_id"]
+    users.logout()
     return redirect("/") 
 
 #register:
@@ -42,23 +44,37 @@ def register():
 
         if users.register(name, password1):
             if users.login(name, password1):
+
+                session["user_id"] = user_id_forname(name)
+                session["name"] = name
                 return redirect("/")
         
 
         return render_template("error.html", message="registeration failed")
 
-#We dont currently use this that much sorry
-@app.route("/allplans")
-def allplans():
-    return "Here we will have the list of all active plans"
 
-@app.route("/allplans/test")
-def test():
-    return "This is a page within a page"
+@app.route("/addplan", methods=["GET", "POST"])
+def add_plan():
+    if request.method == "GET":
+        return render_template("add.html")
+    
+    if request.method == "POST":
+        name = request.form["name"]
+        if len(name) < 1 or len(name) > 20:
+            return render_template("error.html", message="Name should be 1-20 characters long")
+        description = request.form["description"]
+        if len(description) > 1000:
+            return render_template("error.html", message="use shorter description")
+        creator_id = session.get("user_id")
+        plan_id = plans.add_plan(creator_id, name, description, newsubplans=None)
+        return redirect("/plan/"+str(plan_id))
 
-# @app.route("/form")
-# def form():
-#     return render_template("form.html")
+@app.route("/plan/<int:plan_id>")
+def show_plan(plan_id):
+    description = plans.get_plan_info(plan_id)[2]
+    name = plans.get_plan_info(plan_id)[1]
+
+    return render_template("plan.html", id=plan_id, description=description, name=name)
 
 # @app.route("/result", methods=["POST"])
 # def result():
