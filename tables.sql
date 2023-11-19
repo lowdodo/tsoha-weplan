7b67da33e6eeffaf6ad23ebec1f2555356bb21f4
lowdodo <lowdodo@ad.helsinki.fi>
2023-11-19 23:35:13 +0200

    changed users a bit, added 2 new tables

--- tables.sql
+++ tables.sql
@@ -1,1 +1,6 @@
-CREATE TABLE users(userid SERIAL PRIMARY KEY, name TEXT, password TEXT);
+CREATE TABLE users(user_id SERIAL PRIMARY KEY, name TEXT, password TEXT);
+
+CREATE TABLE plans(plan_id SERIAL PRIMARY KEY, creator_id INTEGER REFERENCES users, name TEXT, description TEXT, visible INTEGER);
+
+CREATE TABLE subplans(subplans_id SERIAL PRIMARY KEY, plan_id INTEGER REFERENCES plans, creator_id INTEGER REFERENCES users, name TEXT, description TEXT, visible INTEGER); 
+
