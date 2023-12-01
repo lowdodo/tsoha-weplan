76f5776e4365c42b1abb9a98b53eb4a59ce052e3
lowdodo <lowdodo@ad.helsinki.fi>
2023-12-01 17:15:24 +0200

    broke and fixed some stuff

--- tables.sql
+++ tables.sql
@@ -1,6 +1,30 @@
-CREATE TABLE users(user_id SERIAL PRIMARY KEY, name TEXT, password TEXT);
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    name TEXT,
+    password TEXT
+);
 
-CREATE TABLE plans(plan_id SERIAL PRIMARY KEY, creator_id INTEGER REFERENCES users, name TEXT, description TEXT, visible INTEGER);
+CREATE TABLE plans (
+    plan_id SERIAL PRIMARY KEY,
+    creator_id INTEGER REFERENCES users(user_id), 
+    name TEXT,
+    description TEXT,
+    visible INTEGER
+);
 
-CREATE TABLE subplans(subplans_id SERIAL PRIMARY KEY, plan_id INTEGER REFERENCES plans, creator_id INTEGER REFERENCES users, name TEXT, description TEXT, visible INTEGER); 
+CREATE TABLE subplans (
+    subplans_id SERIAL PRIMARY KEY,
+    plan_id INTEGER REFERENCES plans(plan_id),
+    creator_id INTEGER REFERENCES users(user_id), 
+    name TEXT,
+    description TEXT,
+    visible INTEGER
+);
 
+CREATE TABLE priority (
+    priority_id SERIAL PRIMARY KEY,
+    plan_id INTEGER REFERENCES plans(plan_id),
+    creator_id INTEGER REFERENCES users(user_id), 
+    name INTEGER REFERENCES plans(plan_id),
+    visible INTEGER
+);
