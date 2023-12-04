72797a5aeb88f3b7078cd8515935229c70dda895
lowdodo <lowdodo@ad.helsinki.fi>
2023-12-04 18:29:53 +0200

    fixed showing of subplans

--- routes.py
+++ routes.py
@@ -16,6 +16,7 @@ def login():
     if request.method == "POST":
         name = request.form["name"]
         password = request.form["password"]
+        print("eka sesisom", session)
         if not users.login(name, password):
             return render_template("error.html", message="Incorrect login information")
         return redirect("/")
@@ -64,56 +65,61 @@ def add_plan():
         description = request.form["description"]
         if len(description) > 1000:
             return render_template("error.html", message="use shorter description")
+        print(session)
         creator_id = session.get("user_id")
-        plan_id = plans.add_plan(creator_id, name, description)
+        creator_name = session.get("name")
+        plan_id = plans.add_plan(creator_id, creator_name, name, description)
         flash("new plan added")
         return redirect("/")
 
 @app.route("/addsub/<int:plan_id>", methods=["GET", "POST"])
 def add_subplan(plan_id):
     if request.method == "GET":
-        num_subplans = 2
-        return render_template("addsub.html", plan_id=plan_id, num_subplans=num_subplans)
+        return render_template("addsub.html", plan_id=plan_id)
 
     if request.method == "POST":
-        num_subplans = int(request.form.get("num_subplans", 1))
         creator_id = session.get("user_id")
+        creator_name = session.get("name")
 
-        for i in range(num_subplans):
-            name = request.form[f"name{i}"]
-            description = request.form[f"description{i}"]
 
-            if len(name) < 1 or len(name) > 20:
-                return render_template("error.html", message=f"Name of subplan {i+1} should be 1-20 characters long")
+        name = request.form["name"]
+        description = request.form[f"description"]
 
-            if len(description) > 1000:
-                return render_template("error.html", message=f"Use a shorter description for subplan {i+1}")
+        if len(name) < 1 or len(name) > 20:
+            return render_template("error.html", message=f"Name of subplan should be 1-20 characters long")
 
-            plans.add_subplan(creator_id, plan_id, name, description)
+        if len(description) > 1000:
+            return render_template("error.html", message=f"Use a shorter description")
 
-        flash("new subplans added")
+        plans.add_subplan(creator_id, creator_name, plan_id, name, description)
+
+        flash("new subplan added")
         return redirect(f"/plan/{plan_id}")
     
 @app.route("/plan/<int:plan_id>")
 def show_plan(plan_id):
-    print("hsow pö'sij")
     plan_info = plans.get_plan_info(plan_id)
-    description = plan_info[2]
-    name = plan_info[1]
-    creator = plan_info[0]
+    main_plan = {
+        "creator": plan_info.creator_name,
+        "name": plan_info.name,
+        "description": plan_info.description,
+    }
 
-    print("pääsin tänne asti plan infopn")
-    #is there subplans?
+    #are there subplans?
     subplans_info = plans.get_subplan_info(plan_id)
-    print("TÄSSÄ subinfo", subplans_info)
-    if subplans_info:
-        subdescription = subplans_info[2]
-        subname = subplans_info[1]
-    else:
-        subdescription = None
-        subname = None
-    return render_template("plan.html", plan_id=plan_id, description=description, name=name, creator_id=creator, subplans_info=subplans_info)
 
+    subplans = []
+    for subplan_info in subplans_info:
+        creator_name = subplan_info.creator_name
+        name = subplan_info.name
+        description = subplan_info.description
+        subplans.append({
+            "creator": creator_name,
+            "name": name,
+            "description": description
+        })
+
+    return render_template("plan.html", plan_id=plan_id, main_plan=main_plan, subplans_info=subplans)
 
 @app.route("/removeplan/<int:plan_id>")
 def remove_plan(plan_id):
